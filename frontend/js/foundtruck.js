206495c2172edcf963dd0889eb09f627d0b5e087
Luiz Baldi <baldilp@gmail.com>
2016-09-09 01:18:53 -0300

    ui router implemented instead of ng-view

--- frontend/js/foundtruck.js
+++ frontend/js/foundtruck.js
@@ -1,20 +1,17 @@
-var foundtruck = angular.module('foundtruck', ['ngRoute']);
+var foundtruck = angular.module('foundtruck', ['ui.router']);
 
-foundtruck.config(function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			controller: 'UserLoginController',
-			templateUrl: 'views/user/login.html'
-		})
-		.when('/user/checkposition', {
-			controller: 'CheckPositionController',
-			templateUrl: 'views/user/checkposition.html'
-		})
-		.when('/user/findlocation', {
-			controller: 'FindLocationController',
-			templateUrl: 'views/user/findlocation.html'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
+foundtruck.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider        
+        .state('login', {
+            url: '/',
+            controller: 'UserLoginController',
+            templateUrl: 'views/user/login.html'
+        })
+        .state('findlocation', {
+        	url: '/findlocation',
+        	controller: 'FindLocationController',
+            templateUrl: 'views/user/findlocation.html'         
+        });
 });
