e52e8a2eff98872dc1b49090bcd0ca1a06a5abd8
Luiz Baldi <baldilp@gmail.com>
2017-01-29 15:40:29 -0200

    removing manager module

--- frontend/js/foundtruck.js
+++ frontend/js/foundtruck.js
@@ -3,52 +3,32 @@ var foundtruck = angular.module('foundtruck', ['ui.router', 'LocalStorageModule'
 foundtruck.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/');
 	
-    if (window.appMode == "user") {
-        $stateProvider        
-            .state('userLogin', {
-                url: '/',
-                controller: 'UserLoginController',
-                templateUrl: 'views/user/user-login.html'
-            })
-            .state('findlocation', {
-            	url: '/findlocation',
-            	controller: 'FindLocationController',
-                templateUrl: 'views/user/findlocation.html'         
-            })
-            .state('findlocationalternative',{
-                url: '/findlocationalternative',
-                controller: 'FindLocationAlternativeController',
-                templateUrl: 'views/user/findlocationalternative.html'
-            })
-            .state('loadtrucks', {
-                url: '/loadtrucks',
-                controller: 'LoadTrucksController',
-                templateUrl: 'views/user/loadtrucks.html' 
-            })
-            .state('userRegister', {
-                url: '/userRegister',
-                controller: 'UserRegisterController',
-                templateUrl: 'views/user/user-register.html' 
-            });
-    } else if (window.appMode == "manager") {
-        $stateProvider
-            .state('managerLogin', {
-                url: '/',
-                controller: 'ManagerLoginController',
-                templateUrl: 'views/manager/clientlogin.html'
-            })
-            .state('main',{
-                url:'/main',
-                templateUrl: 'views/manager/main.html'
-            });
-    } else {
-        $stateProvider
-            .state('error',{
-                url: '/',
-                templateUrl: 'views/error.html'
-            });
-    }
-
+    $stateProvider        
+        .state('userLogin', {
+            url: '/',
+            controller: 'UserLoginController',
+            templateUrl: 'views/user-login.html'
+        })
+        .state('findlocation', {
+        	url: '/findlocation',
+        	controller: 'FindLocationController',
+            templateUrl: 'views/findlocation.html'         
+        })
+        .state('findlocationalternative',{
+            url: '/findlocationalternative',
+            controller: 'FindLocationAlternativeController',
+            templateUrl: 'views/findlocationalternative.html'
+        })
+        .state('loadtrucks', {
+            url: '/loadtrucks',
+            controller: 'LoadTrucksController',
+            templateUrl: 'views/loadtrucks.html' 
+        })
+        .state('userRegister', {
+            url: '/userRegister',
+            controller: 'UserRegisterController',
+            templateUrl: 'views/user-register.html' 
+        });
 });
 
 foundtruck.config(['localStorageServiceProvider', function(localStorageServiceProvider) {
